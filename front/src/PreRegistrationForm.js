f9809ead5475c32cd58f3652a192071d4cc1b3ef
stradivirus <stradivirus9@gmail.com>
2024-11-30 08:51:56 +0900

    alert 제거

--- front/src/PreRegistrationForm.js
+++ front/src/PreRegistrationForm.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './PreRegistrationForm.css';
-import SuccessAlert from './components/SuccessAlert';
 
 function PreRegistrationForm() {
   const API_URL = 'http://34.64.132.7:8082';
@@ -12,9 +11,6 @@ function PreRegistrationForm() {
   const [isRegistered, setIsRegistered] = useState(false);
   const [emailError, setEmailError] = useState('');
   const [phoneError, setPhoneError] = useState('');
-  const [showAlert, setShowAlert] = useState(false);
-  const [alertMessage, setAlertMessage] = useState('');
-  const [alertCreatedAt, setAlertCreatedAt] = useState('');
   const navigate = useNavigate();
 
   const handleEmailChange = (e) => {
@@ -95,9 +91,6 @@ function PreRegistrationForm() {
         const data = await response.json();
         setCouponCode(data.coupon_code);
         setIsRegistered(true);
-        setAlertMessage(data.message);
-        setAlertCreatedAt(data.created_at);
-        setShowAlert(true);
       } else {
         const errorData = await response.json();
         alert(`오류: ${errorData.detail}`);
@@ -116,9 +109,6 @@ function PreRegistrationForm() {
     setIsRegistered(false);
     setEmailError('');
     setPhoneError('');
-    setShowAlert(false);
-    setAlertMessage('');
-    setAlertCreatedAt('');
   };
 
   const handleUseCoupon = () => {
@@ -147,14 +137,6 @@ function PreRegistrationForm() {
 
   return (
     <div className="pre-registration-container">
-      {showAlert && (
-        <SuccessAlert 
-          message={alertMessage}
-          createdAt={alertCreatedAt}
-          onClose={() => setShowAlert(false)}
-        />
-      )}
-
       <div className="form-header">
         <p>축제 소식을 가장 먼저 받아보세요!</p>
       </div>
