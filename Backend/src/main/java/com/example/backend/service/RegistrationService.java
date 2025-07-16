837ad9eb8a11a353b8387ecfd0350ac4776dd85d
root <root@DESKTOP-I98R5CS.localdomain>
2025-07-16 12:17:01 +0900

    안 쓰는 코드 삭제

--- Backend/src/main/java/com/example/backend/service/RegistrationService.java
+++ Backend/src/main/java/com/example/backend/service/RegistrationService.java
@@ -4,8 +4,6 @@ import com.example.backend.entity.Registration;
 import com.example.backend.repository.RegistrationRepository;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
 @Service
 public class RegistrationService {
     private final RegistrationRepository registrationRepository;
